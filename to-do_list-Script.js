9815baa7e72cad013e1f1225c04b81a9af5fc1c2
dee-rius <darkwahdarius10@gmail.com>
2025-04-04 18:48:56 +0100

    Made it so deleting a task doesn't make it reappear

--- to-do_list-Script.js
+++ to-do_list-Script.js
@@ -6,177 +6,165 @@ const confirmButton = document.getElementById("confirm-button");
 confirmButton.addEventListener('click', createTask);
 
 let currentTasksArray = [];
-let storedTask = []
-if(localStorage.getItem("current-tasks") != null)
-{
+let storedTask = [];
+if (localStorage.getItem("current-tasks") != null) {
     storedTasks = JSON.parse(localStorage.getItem("current-tasks"));
     currentTasksArray = storedTasks;
 
-    for(let storedTask of storedTasks)
-    {
-       const taskContainer = document.createElement("section");
-       taskContainer.classList.add("task-container");
-       taskContainer.style.display = "flex";
+    for (let storedTask of storedTasks) {
+        let taskContainer = document.createElement("article");
+        taskContainer.classList.add("task-container");
+        taskContainer.style.display = "flex";
 
-       const taskDetails = document.createElement("article");
-       taskDetails.style.wordBreak = "break-word";
+        let taskDetails = document.createElement("article");
+        taskDetails.style.wordBreak = "break-word";
 
-       const taskCompleteCheckbox = document.createElement("input");
-       taskCompleteCheckbox.type = "checkbox";
+        let taskCompleteCheckbox = document.createElement("input");
+        taskCompleteCheckbox.type = "checkbox";
 
-       taskCompleteCheckbox.style.marginBottom = "auto";
-       
-       const task = document.createElement("p");
-       const taskDescription = document.createElement("p");
+        taskCompleteCheckbox.style.marginBottom = "auto";
+
+        let task = document.createElement("p");
+        let taskDescription = document.createElement("p");
 
 
-       task.classList.add("task-details-text");
-       taskDescription.classList.add("task-details-text");
+        task.classList.add("task-details-text");
+        taskDescription.classList.add("task-details-text");
 
-       //set to saved task input value
-       task.textContent = storedTask.taskName;
-       //set to saved task dscription value
-       taskDescription.textContent = storedTask.description;
+        //set to saved task input value
+        task.textContent = storedTask.taskName;
+        //set to saved task dscription value
+        taskDescription.textContent = storedTask.description;
 
-       const deleteTaskButton = document.createElement("button");
+        let deleteTaskButton = document.createElement("button");
 
-       deleteTaskButton.style.marginBottom = "auto";
-       deleteTaskButton.classList.add("delete-task");
-       deleteTaskButton.innerHTML = "delete";
+        deleteTaskButton.style.marginBottom = "auto";
+        deleteTaskButton.classList.add("delete-task");
+        deleteTaskButton.innerHTML = "delete";
 
-       taskCompleteCheckbox.addEventListener('change', taskCompletion);
-       deleteTaskButton.addEventListener('click', playTaskExitAnim);
+        taskCompleteCheckbox.addEventListener('change', taskCompletion);
+        deleteTaskButton.addEventListener('click', playTaskExitAnim);
 
-       taskDetails.append(task, taskDescription);
-       taskContainer.append(taskCompleteCheckbox, taskDetails, deleteTaskButton);
-       allTasksHolder.appendChild(taskContainer);
+        taskDetails.append(task, taskDescription);
+        taskContainer.append(taskCompleteCheckbox, taskDetails, deleteTaskButton);
+        allTasksHolder.appendChild(taskContainer);
 
-       taskContainer.classList.add("task-enter");
+        taskContainer.classList.add("task-enter");
     }
 }
 
 
 
-function createTask()
-{
+function createTask() {
     let inputtedTask = taskInput.value.trim();
-    
-
-    if(inputtedTask != "")
-    {
-       let taskToStore = 
-       {
-        taskName: taskInput.value,
-        description: taskDescriptionInput.value,
-       };
-       const taskContainer = document.createElement("section");
-       taskContainer.classList.add("task-container");
-       taskContainer.style.display = "flex";
-
-       const taskDetails = document.createElement("article");
-       taskDetails.style.wordBreak = "break-word";
-
-       const taskCompleteCheckbox = document.createElement("input");
-       taskCompleteCheckbox.type = "checkbox";
-
-       taskCompleteCheckbox.style.marginBottom = "auto";
-       
-       const task = document.createElement("p");
-       const taskDescription = document.createElement("p");
 
 
-       task.classList.add("task-details-text");
-       taskDescription.classList.add("task-details-text");
+    if (inputtedTask != "") {
+        let taskToStore =
+        {
+            taskName: taskInput.value,
+            description: taskDescriptionInput.value,
+        };
 
-       task.textContent = taskInput.value;
-       taskDescription.textContent = taskDescriptionInput.value;
+        let taskContainer = document.createElement("article");
+        taskContainer.classList.add("task-container");
+        taskContainer.style.display = "flex";
 
-       const deleteTaskButton = document.createElement("button");
+        let taskDetails = document.createElement("article");
+        taskDetails.style.wordBreak = "break-word";
 
-       deleteTaskButton.style.marginBottom = "auto";
-       deleteTaskButton.classList.add("delete-task");
-       deleteTaskButton.innerHTML = "delete";
+        let taskCompleteCheckbox = document.createElement("input");
+        taskCompleteCheckbox.type = "checkbox";
 
-       taskCompleteCheckbox.addEventListener('change', taskCompletion);
-       deleteTaskButton.addEventListener('click', playTaskExitAnim);
+        taskCompleteCheckbox.style.marginBottom = "auto";
 
-       taskDetails.append(task, taskDescription);
-       taskContainer.append(taskCompleteCheckbox, taskDetails, deleteTaskButton);
-       allTasksHolder.appendChild(taskContainer);
-
-       taskContainer.classList.add("task-enter");
+        let task = document.createElement("p");
+        let taskDescription = document.createElement("p");
 
 
-       currentTasksArray.splice(currentTasksArray.length, 0, taskToStore);
-       
+        task.classList.add("task-details-text");
+        taskDescription.classList.add("task-details-text");
 
-       localStorage.setItem('current-tasks', JSON.stringify(currentTasksArray));
+        task.textContent = taskInput.value;
+        taskDescription.textContent = taskDescriptionInput.value;
+
+        let deleteTaskButton = document.createElement("button");
+
+        deleteTaskButton.style.marginBottom = "auto";
+        deleteTaskButton.classList.add("delete-task");
+        deleteTaskButton.innerHTML = "delete";
+
+        taskCompleteCheckbox.addEventListener('change', taskCompletion);
+        deleteTaskButton.addEventListener('click', playTaskExitAnim);
+
+        taskDetails.append(task, taskDescription);
+        taskContainer.append(taskCompleteCheckbox, taskDetails, deleteTaskButton);
+        allTasksHolder.appendChild(taskContainer);
+
+        taskContainer.classList.add("task-enter");
+
+
+        currentTasksArray.splice(currentTasksArray.length, 0, taskToStore);
+
+
+        localStorage.setItem('current-tasks', JSON.stringify(currentTasksArray));
+
     }
 }
 
-function playTaskExitAnim()
-{
-    const taskContainers = document.getElementsByClassName("task-container");
-    
 
-    for(const taskContainer of taskContainers)
-    {
-        if(taskContainer.contains(this))
-        {
-          const taskDetails = taskContainer.getElementsByClassName("task-details-text");
-          
+function playTaskExitAnim() {
+    let taskContainers = document.getElementsByClassName("task-container");
+    let storedTasks = JSON.parse(localStorage.getItem('current-tasks'));
+    let deletedOneTask = false;
+
+    for (let taskContainer of taskContainers) {
+        if (taskContainer.contains(this)) {
+
+            let taskDetails = taskContainer.getElementsByClassName("task-details-text");
+
             taskContainer.classList.remove("task-enter");
             taskContainer.classList.add("task-exit");
 
-            setTimeout(deleteTask, 100);
+            for (storedTask of storedTasks) {
+                let storedTaskIndex = storedTasks.findIndex(storedTask=> storedTask.taskName == taskDetails[0].textContent && storedTask.description == taskDetails[1].textContent);
 
-            function deleteTask()
-            {
-                taskContainer.remove();
-            }
-            
-            for(storedTask of storedTasks)
-            {
-              let storedTaskIndex = storedTasks.findIndex(storedTask => storedTask.taskName == taskDetails[0].textContent && storedTask.description == taskDetails[1].textContent);
-              
-              if(storedTask.taskName == taskDetails[0].textContent && storedTask.description == taskDetails[1].textContent)
-              {
-                storedTasks.splice(storedTaskIndex, 1);
-                console.log(storedTasks)
-                
-                localStorage.setItem('current-tasks', JSON.stringify(storedTasks));
-              }
-              else
-              {
-                console.log("...")
-              }
-            }
+                if (storedTask.taskName == taskDetails[0].textContent && storedTask.description == taskDetails[1].textContent && deletedOneTask == false)  {
+                    storedTasks.splice(storedTaskIndex, 1);
+                    console.log(storedTasks);
+
+                    localStorage.setItem('current-tasks', JSON.stringify(storedTasks));
+                    deletedOneTask = true;
+
+                    setTimeout(deleteTask, 100);
+
+                    function deleteTask() {
+                        taskContainer.remove();
+                    }
+                }
+                else {
+                    console.log("...");
+                }
             }
         }
     }
+}
 
 
 
-function taskCompletion()
-{
-    const taskContainers = document.getElementsByClassName("task-container");
+function taskCompletion() {
+    let taskContainers = document.getElementsByClassName("task-container");
 
-    if(this.checked)
-    {
-        for(const taskContainer of taskContainers)
-        {
-            if(taskContainer.contains(this))
-            {
+    if (this.checked) {
+        for (let taskContainer of taskContainers) {
+            if (taskContainer.contains(this)) {
                 taskContainer.classList.add("task-complete");
             }
         }
     }
-    else
-    {
-        for(const taskContainer of taskContainers)
-        {
-            if(taskContainer.contains(this))
-            {
+    else {
+        for (let taskContainer of taskContainers) {
+            if (taskContainer.contains(this)) {
                 taskContainer.classList.remove("task-complete");
             }
         }
